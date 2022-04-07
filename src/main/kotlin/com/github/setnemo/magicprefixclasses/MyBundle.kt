c5e2fda37c731514b1bb76ffdbdf4afcd9af8a91
GitHub Action <action@github.com>
2022-04-07 16:01:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/setnemo/magicprefixclasses/MyBundle.kt
+++ src/main/kotlin/com/github/setnemo/magicprefixclasses/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.setnemo.magicprefixclasses
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/setnemo/magicprefixclasses/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.setnemo.magicprefixclasses.services
+
+import com.github.setnemo.magicprefixclasses.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
